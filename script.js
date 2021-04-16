ddc2309cf4cbd0480039692fa3331de9dc05f0e7
Michael <mccarlton95@gmail.com>
2021-04-16 14:27:46 +0100

    making stepps i think

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var fithteen = 1000;
 var time = document.getElementById("time");
-setInterval(countDown, 0.1);
 
-//document.getElementById("start").onclick = 
+document.getElementById("start").onclick =
     function down() {
     for (var i = 0; i < fithteen;) {
     fithteen--;
     time.textContent = fithteen;
+    setInterval(down, 0.1);
     settimeout(1);
     }
 }
