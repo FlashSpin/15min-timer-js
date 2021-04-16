75aa5c81013b18139028f636b3169225a7874cdd
Michael <mccarlton95@gmail.com>
2021-04-16 12:00:30 +0100

    nerly crashed!

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-var fithteen = 900000;
+var fithteen = 9;
 var time = document.getElementById("time");
 time.textContent = fithteen;
 
+for (var i = 0; i < fithteen; i++) {
+    fithteen--;
+    console.log(time
+}
 console.log(time);
